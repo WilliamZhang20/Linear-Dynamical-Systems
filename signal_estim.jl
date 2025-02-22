d747d3bc8f699210eda18bc6323904db259526d0
William Zhang <wzhang20@yahoo.com>
2025-02-21 22:47:27 -0500

    finish signal estimation question

--- signal_estim.jl
+++ signal_estim.jl
@@ -38,18 +38,30 @@ col_1 = zeros(n*k)
 c_j = [r(j) for j = 0:q]
 col_1[1:length(c_j)] = c_j
 
-B = Toeplitz(row_1, col_1)
+B = Toeplitz(row_1, col_1) # y = B*u + w
 display(B)
 
 # c) x_reg (regularized least squares)
 
 A = B*C
 
-x_reg = inv(A'*A + mu*I(n))*A'*y # apostrophe = transpose
+x_reg = inv(A'*A + mu*I(n))*A'*y # Added a term of the identity matrix to regularize
 
 plot(x, label="x")
 plot!(x_reg, label = "x_reg")
-savefig("..\\graphics\\Reg_LS.pdf")
+savefig(".\\graphics\\Reg_LS.pdf")
 
 # d) Recursive Method
 
+P = mu * I(n)
+q = zeros(n)
+for i = 1:k*n
+    global P += A[i, :]*A[i, :]'
+    global q += y[i]*A[i, :]
+end
+
+x_rec = inv(P)*q
+
+plot(x, label="x")
+plot!(x_reg, label = "x_recur")
+savefig(".\\graphics\\Recursive_LS.pdf")
